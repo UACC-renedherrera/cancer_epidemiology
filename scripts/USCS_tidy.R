231e65109aedafd1ac4dcd44ffddfcd26bdc75e9
Rene Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2020-06-05 18:29:31 -0700

    updated USCS_tidy.R and accompanying output

--- scripts/USCS_tidy.R
+++ scripts/USCS_tidy.R
@@ -1,12 +1,32 @@
+# set up
+# load packages to read and tidy data
 library(here)
 library(tidyverse)
+library(purrr)
+
+# source citation
+# National Program of Cancer Registries and Surveillance, Epidemiology, and End Results SEER*Stat Database: NPCR and SEER Incidence – U.S. Cancer Statistics 2001–2016 Public Use Research Database, November 2018 submission (2001–2016), United States Department of Health and Human Services, Centers for Disease Control and Prevention and National Cancer Institute. Released June 2019, based on the November 2018 submission. Accessed at www.cdc.gov/cancer/uscs/public-use.
+
+# download data from source
+# download dataset
+# set values
+url <- "https://www.cdc.gov/cancer/uscs/USCS-1999-2016-ASCII.zip"
+path_zip <- "data/raw"
+path_unzip <- "data/raw/USCS_1999-2016"
+zip_file <- "USCS_1999-2016_ASCII.zip"
+# use curl to download 
+curl::curl_download(url, destfile = paste(path_zip, zip_file, sep = "/"))
+#set value
+zipped_file <- "data/raw/USCS_1999-2016_ASCII.zip"
+# unzip to folder
+unzip(zipped_file, exdir = path_unzip)
 
 # read data ----
 # United States Cancer Statistics
 # for years 1999-2016
 # by cancer site ----
 
-by_cancer <- read_delim("../Cancer_Data_Sources/data_raw/USCS/BYSITE.TXT",
+by_cancer <- read_delim("data/raw/USCS_1999-2016/BYSITE.TXT",
   delim = "|",
   na = c(" ", "", "NA", "~", "."),
   col_names = TRUE,
@@ -30,8 +50,11 @@ by_cancer <- read_delim("../Cancer_Data_Sources/data_raw/USCS/BYSITE.TXT",
 
 write_rds(by_cancer, "data/tidy/USCS_by_cancer.rds")
 
+# read data
+# United States Cancer Statistics
+# for years 1999-2016
 # by age, race, and ethnicity ----
-by_age <- read_delim("../Cancer_Data_Sources/data_raw/USCS/BYAGE.TXT",
+by_age <- read_delim("data/raw/USCS_1999-2016/BYAGE.TXT",
   delim = "|",
   na = c(" ", "", "NA", "~", ".", "+", "-"),
   col_names = TRUE,
@@ -53,8 +76,11 @@ by_age <- read_delim("../Cancer_Data_Sources/data_raw/USCS/BYAGE.TXT",
 
 write_rds(by_age, "data/tidy/USCS_by_age.rds")
 
+# read data
+# United States Cancer Statistics
+# for years 1999-2016
 # by state and region ----
-by_state <- read_delim("../Cancer_Data_Sources/data_raw/USCS/BYAREA.TXT",
+by_state <- read_delim("data/raw/USCS_1999-2016/BYAREA.TXT",
   delim = "|",
   na = c(" ", "", "NA", "~", ".", "+", "-"),
   col_names = TRUE,
@@ -79,8 +105,11 @@ by_state <- read_delim("../Cancer_Data_Sources/data_raw/USCS/BYAREA.TXT",
 
 write_rds(by_state, "data/tidy/USCS_by_state.rds")
 
+# read data
+# United States Cancer Statistics
+# for years 1999-2016
 # by state and county ----
-by_county <- read_delim("../Cancer_Data_Sources/data_raw/USCS/BYAREA_COUNTY.TXT",
+by_county <- read_delim("data/raw/USCS_1999-2016/BYAREA_COUNTY.TXT",
   delim = "|",
   na = c(" ", "", "NA", "~", ".", "+", "-"),
   col_names = TRUE,
@@ -108,16 +137,6 @@ by_az_county <- by_county %>%
   filter(STATE == "AZ")
 
 write_rds(by_az_county, "data/tidy/USCS_by_az_county.rds")
-
-# save sample dataset
-sample_by_age <- sample_n(by_age, 100)
-sample_by_cancer <- sample_n(by_cancer, 100)
-sample_by_state <- sample_n(by_state, 100)
-
-# write sample to file
-write_rds(sample_by_age, "data/samples/USCS_sample_by_age.rds")
-write_rds(sample_by_cancer, "data/samples/USCS_sample_by_cancer.rds")
-write_rds(sample_by_state, "data/samples/USCS_sample_by_state.rds")
 
 # explore by_age ----
 
@@ -197,4 +216,84 @@ by_cancer %>%
     subtitle = "years 2012-2016, 20 most common cancers"
   )
 
-levels(by_cancer$SITE)
+# explore by_az_county ----
+str(by_az_county)
+# incidence
+by_az_county %>%
+  filter(
+    EVENT_TYPE == "Incidence",
+    RACE == "All Races",
+    SEX == "Male and Female",
+    SITE == "All Cancer Sites Combined",
+    YEAR == "2012-2016"
+  ) %>%
+  drop_na() %>%
+  arrange(desc(AGE_ADJUSTED_RATE)) %>%
+  ggplot(mapping = aes(x = AGE_ADJUSTED_RATE, y = reorder(AREA, AGE_ADJUSTED_RATE))) +
+  geom_errorbarh(aes(xmin = AGE_ADJUSTED_CI_LOWER, xmax = AGE_ADJUSTED_CI_UPPER)) +
+  geom_bar(aes(x = AGE_ADJUSTED_RATE), stat = "identity", alpha = 0.5) +
+  labs(
+    title = "age adjusted rate of new cancers for AZ Counties",
+    subtitle = "years 2012-2016"
+  )
+
+# mortality
+by_az_county %>%
+  filter(
+    EVENT_TYPE == "Mortality",
+    RACE == "All Races",
+    SEX == "Male and Female",
+    SITE == "All Cancer Sites Combined",
+    YEAR == "2012-2016"
+  ) %>%
+  drop_na() %>%
+  arrange(desc(AGE_ADJUSTED_RATE)) %>%
+  ggplot(mapping = aes(x = AGE_ADJUSTED_RATE, y = reorder(AREA, AGE_ADJUSTED_RATE))) +
+  geom_errorbarh(aes(xmin = AGE_ADJUSTED_CI_LOWER, xmax = AGE_ADJUSTED_CI_UPPER)) +
+  geom_bar(aes(x = AGE_ADJUSTED_RATE), stat = "identity", alpha = 0.5) +
+  labs(
+    title = "age adjusted rate of cancer deaths for AZ Counties",
+    subtitle = "years 2012-2016"
+  )
+
+# compare USA to AZ
+# explore by_state ----
+str(by_state)
+# incidence
+by_state %>%
+  filter(
+    EVENT_TYPE == "Incidence",
+    RACE == "All Races",
+    SEX == "Male and Female",
+    SITE == "All Cancer Sites Combined",
+    YEAR == "2012-2016"
+  ) %>%
+  drop_na() %>%
+  arrange(desc(AGE_ADJUSTED_RATE)) %>%
+  ggplot(mapping = aes(x = AGE_ADJUSTED_RATE, y = reorder(AREA, AGE_ADJUSTED_RATE))) +
+  geom_errorbarh(aes(xmin = AGE_ADJUSTED_CI_LOWER, xmax = AGE_ADJUSTED_CI_UPPER)) +
+  geom_bar(aes(x = AGE_ADJUSTED_RATE), stat = "identity", alpha = 0.5) +
+  labs(
+    title = "age adjusted rate of new cancers for each USA state",
+    subtitle = "years 2012-2016"
+  )
+
+# mortality
+by_state %>%
+  filter(
+    EVENT_TYPE == "Mortality",
+    RACE == "All Races",
+    SEX == "Male and Female",
+    SITE == "All Cancer Sites Combined",
+    YEAR == "2012-2016"
+  ) %>%
+  drop_na() %>%
+  arrange(desc(AGE_ADJUSTED_RATE)) %>%
+  ggplot(mapping = aes(x = AGE_ADJUSTED_RATE, y = reorder(AREA, AGE_ADJUSTED_RATE))) +
+  geom_errorbarh(aes(xmin = AGE_ADJUSTED_CI_LOWER, xmax = AGE_ADJUSTED_CI_UPPER)) +
+  geom_bar(aes(x = AGE_ADJUSTED_RATE), stat = "identity", alpha = 0.5) +
+  labs(
+    title = "age adjusted rate of cancer deaths for each USA state",
+    subtitle = "years 2012-2016"
+  )
+
