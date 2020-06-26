148229655f7284fafeb02230eaa5e5c8d0e66404
Rene Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2020-06-26 12:36:30 -0700

    pima county mortality table and plot; update filter

--- scripts/seer_stat_data_processing.R
+++ scripts/seer_stat_data_processing.R
@@ -1,13 +1,13 @@
 # seer stat exports
 
-# set up
+# set up ----
 # load packages
 library(here)
 library(tidyverse)
 library(knitr)
 library(ggthemes)
 
-# read dataset
+# read dataset mortality ----
 catchment_mortality <- read_rds("data/tidy/seer_stat_catchment_mortality_2013-2017.rds")
 
 # determine top five cancers for each sex grouping 
@@ -37,14 +37,18 @@ catchment_mortality %>%
   facet_wrap("sex") +
   theme_solarized()
 
-# read dataset
+# read dataset hispanic mortality ---- 
 catchment_mortality_hispanic <- read_rds("data/tidy/seer_stat_catchment_mortality_hispanic_2013-2017.rds")
 
 # determine top five cancers for each sex grouping 
 # table
 catchment_mortality_hispanic %>% 
   group_by(sex) %>%
-  filter(cancer != "All Causes of Death" & cancer != "All Malignant Cancers") %>%
+  filter(cancer != "Miscellaneous Malignant Cancer" &
+           cancer != "Colon excluding Rectum" &
+           cancer != "All Causes of Death" & 
+           cancer != "All Malignant Cancers" & 
+           !(cancer %in% cancer_to_exclude)) %>%
   select(sex, cancer, age_adjusted_rate) %>%
   arrange(desc(age_adjusted_rate)) %>%
   slice(1:5) %>%
@@ -54,7 +58,11 @@ catchment_mortality_hispanic %>%
 # plot
 catchment_mortality_hispanic %>%
   group_by(sex) %>%
-  filter(cancer != "All Causes of Death" & cancer != "All Malignant Cancers") %>%
+  filter(cancer != "Miscellaneous Malignant Cancer" &
+           cancer != "Colon excluding Rectum" &
+           cancer != "All Causes of Death" & 
+           cancer != "All Malignant Cancers" & 
+           !(cancer %in% cancer_to_exclude)) %>%
   arrange(desc(age_adjusted_rate)) %>%
   slice(1:5) %>%
   ggplot(mapping = aes(x = age_adjusted_rate, y = reorder(cancer, age_adjusted_rate))) +
@@ -62,6 +70,49 @@ catchment_mortality_hispanic %>%
   labs(title = "Top 5 Cancer Deaths in Catchment for years 2013-2017",
        subtitle = "hispanic only ",
        x = "Age Adjusted Rate per 100,000",
+       y = "",
+       caption = "Source: Surveillance, Epidemiology, and End Results (SEER) Program (www.seer.cancer.gov)") +
+  facet_wrap("sex") +
+  theme_solarized()
+
+# read dataset pima mortality ----
+pima_mortality_by_cancer <- read_rds("data/tidy/seer_stat_pima_mortality_2013-2017.rds")
+
+# determine top five cancers for each sex grouping 
+# filter out "system"
+# and save to value for use later
+cancer_to_exclude <- str_subset(pima_mortality_by_cancer$cancer, " System")
+
+# table
+pima_mortality_by_cancer %>% 
+  group_by(sex) %>%
+  filter(cancer != "Miscellaneous Malignant Cancer" &
+           cancer != "Colon excluding Rectum" &
+           cancer != "All Causes of Death" & 
+           cancer != "All Malignant Cancers" & 
+           !(cancer %in% cancer_to_exclude)) %>%
+  select(sex, cancer, age_adjusted_rate) %>%
+  arrange(desc(age_adjusted_rate)) %>%
+  slice(1:5) %>%
+  kable(caption = "Age adjusted mortality rate per 100,000 for five county catchment, for all races combined, for all sexes, and all cancer, year 2013-2017",
+        col.names = c("Sex", "Cancer", "Age Adjusted Rate per 100,000"))
+
+# plot 
+pima_mortality_by_cancer %>% 
+  group_by(sex) %>%
+  filter(cancer != "Miscellaneous Malignant Cancer" &
+           cancer != "Colon excluding Rectum" &
+           cancer != "All Causes of Death" & 
+           cancer != "All Malignant Cancers" & 
+           !(cancer %in% cancer_to_exclude)) %>%
+  select(sex, cancer, age_adjusted_rate) %>%
+  arrange(desc(age_adjusted_rate)) %>%
+  slice(1:5) %>%
+  ggplot(mapping = aes(x = age_adjusted_rate, y = reorder(cancer, age_adjusted_rate))) +
+  geom_col(position = "dodge", alpha = 0.5) +
+  labs(title = "Top 5 Cancer Deaths in Pima County, AZ",
+       subtitle = "Five year average 2013-2017; All races",
+       x = "Age Adjusted Mortality Rate per 100,000",
        y = "",
        caption = "Source: Surveillance, Epidemiology, and End Results (SEER) Program (www.seer.cancer.gov)") +
   facet_wrap("sex") +
