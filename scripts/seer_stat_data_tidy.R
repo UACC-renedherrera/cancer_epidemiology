3729a8e9e45bd553f0f205bd987293c50228394e
Rene Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2020-06-26 10:38:07 -0700

    tidy pima mortality

--- scripts/seer_stat_data_tidy.R
+++ scripts/seer_stat_data_tidy.R
@@ -1,6 +1,6 @@
 # seer stat exports
 
-# set up
+# set up ----
 # load packages
 library(here)
 library(tidyverse)
@@ -41,7 +41,7 @@ glimpse(catchment_mortality)
 str(catchment_mortality$cancer)
 unique(catchment_mortality$cancer)
 
-# five county catchment, hispanic 
+# five county catchment, hispanic ----
 # {Age at Death.Age recode with <1 year olds} = '00 years','01-04 years','05-09 years','10-14 years','15-19 years','20-24 years','25-29 years','30-34 years','35-39 years','40-44 years','45-49 years','50-54 years','55-59 years','60-64 years','65-69 years','70-74 years','75-79 years','80-84 years','85+ years','Unknown'
 # {Race, Sex, Year Dth, State, Cnty, Reg.Race recode (W, B, AI, API)} = 'White','Black','American Indian/Alaska Native','Asian or Pacific Islander'
 # AND {Race, Sex, Year Dth, State, Cnty, Reg.Sex} = 'Male and female','  Male','  Female'
@@ -80,3 +80,38 @@ write_rds(catchment_mortality_hispanic, "data/tidy/seer_stat_catchment_mortality
 glimpse(catchment_mortality_hispanic)
 str(catchment_mortality_hispanic$cancer)
 unique(catchment_mortality_hispanic$cancer)
+
+# pima county mortality ---- 
+# {Age at Death.Age recode with <1 year olds} = '00 years','01-04 years','05-09 years','10-14 years','15-19 years','20-24 years','25-29 years','30-34 years','35-39 years','40-44 years','45-49 years','50-54 years','55-59 years','60-64 years','65-69 years','70-74 years','75-79 years','80-84 years','85+ years'
+# {Race, Sex, Year Dth, State, Cnty, Reg.Race recode (W, B, AI, API)} = 'White','Black','American Indian/Alaska Native','Asian or Pacific Islander'
+# AND {Race, Sex, Year Dth, State, Cnty, Reg.Sex} = 'Male and female'
+# AND {Race, Sex, Year Dth, State, Cnty, Reg.Year of death recode} = '2013-2017'
+# AND {Race, Sex, Year Dth, State, Cnty, Reg.State} = 'Arizona'
+# AND {Race, Sex, Year Dth, State, Cnty, Reg.State-county} = '  AZ: Pima County (04019)'
+# {Site and Morphology.Cause of death recode} = '  All Malignant Cancers'
+
+# read data from saved export 
+pima_mortality_by_cancer <- read_delim("data/raw/seer_stat/mortality_pima_all_race_all_sex_2013-2017.txt",
+                                           delim = ",",
+                                           col_names = c("cancer",
+                                                         "sex",
+                                                         "age_adjusted_rate",
+                                                         "standard_error",
+                                                         "lower_ci",
+                                                         "upper_ci",
+                                                         "case_count",
+                                                         "population"),
+                                           col_types = cols("cancer" = col_factor(),
+                                                            "sex" = col_factor(),
+                                                            "age_adjusted_rate" = col_number(),
+                                                            "standard_error" = col_number(),
+                                                            "lower_ci" = col_number(),
+                                                            "upper_ci" = col_number(),
+                                                            "case_count" = col_number(),
+                                                            "population" = col_number()),
+                                           na = c("", "^", "NA"))
+
+pima_mortality_by_cancer <- drop_na(pima_mortality_by_cancer)
+
+#save dataset
+write_rds(pima_mortality_by_cancer, "data/tidy/seer_stat_pima_mortality_2013-2017.rds")
