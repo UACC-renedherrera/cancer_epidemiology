27bf3a8ebc1f78e8340130c70c6c6b2e5cae95c6
Rene Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2020-07-08 19:28:53 -0700

    pima incidence by sex; by age

--- scripts/AZDHS_data_tidy.R
+++ scripts/AZDHS_data_tidy.R
@@ -364,7 +364,47 @@ write_rds(pima_incidence_by_race, "data/tidy/azdhs_pima_2013-2017_incidence_by_r
 # use datamaid package to generate codebook
 makeCodebook(pima_incidence_by_race, file = "data/tidy/codebook_azdhs_pima_incidence_2013-2017_by_cancer.Rmd")
 
-# by sex
-# by age?
+# pima county by sex ----
+# see above 
+pima_incidence_by_cancer
 
+# pima county by age ---- 
+# Query Item	Description / Value
+# Navigation Path	IBIS-PH > Custom Query > AzCR > Count of Incident Cancer
+# Module	Arizona Cancer Registry Query Module
+# Measure	Count of Incident Cancer (04/28/2020)
+# Time of Query	Wed, Jul 8, 2020 7:19 PM, MST
+# Year Filter	2017, 2016, 2015, 2014, 2013
+# Cancer Sites Filter	Cancer Sites, Oral Cavity, Esophagus, Stomach, Small Intestine, Colorectal, Anus, Anal Canal and Anorectum, Liver and Intrahepatic Bile Duct, Gallbladder and Other Biliary, Pancreas, Larynx, Lung and Bronchus, Bones and Joints, Cutaneous Melanoma, Breast Invasive, Breast In Situ, Corpus Uteri and Uterus, NOS, Cervix Uteri, Ovary, Prostate, Testis, Urinary Bladder, Kidney/Renal Pelvis, Brain and Other Nervous System, Thyroid, Hodgkins Lymphoma, Non-Hodgkins Lymphoma, Myeloma, Leukemia, Mesothelioma, Kaposi Sarcoma, Other Invasive
+# Age Group Filter	0-4 years, 5-9 years, 10-14 years, 15-19 years, 20-24 years, 25-29 years, 30-34 years, 35-39 years, 40-44 years, 45-49 years, 50-54 years, 55-59 years, 60-64 years, 65-69 years, 70-74 years, 75-79 years, 80-84 years, 85+ years
+# Race Filter	White, Non Hispanic, White, Hispanic, Black, American Indian, Asian/Pacific Islander, Other
+# County Filter	County, Pima
+# Data Grouped By	Age Group, Sex
 
+# read data
+pima_incidence_by_age <- read_csv("data/raw/AZDHS/query_pima_incidence_2013-2017_by_age.csv",
+                                  na = c("", "NA", "**", "*"),
+                                  skip = 1,
+                                  col_names = c(
+                                    "Age_Group",
+                                    "Sex",
+                                    "Case_Count"
+                                  ),
+                                  col_types = cols(
+                                    "Age_Group" = col_factor(ordered = TRUE),
+                                    "Sex" = col_factor(ordered = TRUE),
+                                    "Case_Count" = col_number()
+                                  ),
+)
+
+# add variables to define dataset
+pima_incidence_by_age <- pima_incidence_by_age %>%
+  mutate(
+    Year = "2013-2017",
+    Cancer = "All Cancers Combined"
+  )
+
+pima_incidence_by_age$Sex <- fct_recode(pima_incidence_by_age$Sex, "Male and Female" = "All")
+
+# save dataset to file
+write_rds(pima_incidence_by_age, "data/tidy/azdhs_pima_2013-2017_incidence_by_age.rds")
