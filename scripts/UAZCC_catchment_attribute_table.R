84807234f5a54c08e14ba2f6664c144fa80d1075
Rene Dario Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2020-08-21 18:58:27 -0700

    prepare data for uazcc table

--- scripts/UAZCC_catchment_attribute_table.R
+++ scripts/UAZCC_catchment_attribute_table.R
@@ -1,62 +1,89 @@
-# rationale ---- 
+# rationale ----
 # use this script to combine values from both USCS & ADHS
+# to form data table for UAZCC characterization
 
-# set up ---- 
-# load packages 
+# set up ----
+# load packages
 library(here)
 library(tidyverse)
 library(knitr)
 
-# incidence ---- 
-# read dataset USCS ---- 
-USCS_incidence_USA_AZ <- read_rds("data/tidy/USCS_incidence_2012_2016_USA_AZ.rds")
+# incidence ----
+# read dataset USCS ----
+incidence_uscs <- read_rds("data/tidy/incidence_us_uscs_2013-2017_by_cancer.rds")
 
-# read dataset ADHS ---- 
-ADHS_incidence_catch <- read_rds("data/tidy/incidence_azdhs_az_catch_race_2012-2016.rds")
+glimpse(incidence_uscs)
+str(incidence_uscs)
 
-# recode ADHS to match USCS ---- 
-ADHS_incidence_catch <- ADHS_incidence_catch %>%
-  mutate(Cancer = as_factor(Cancer)) %>%
-  mutate(Cancer = fct_recode(Cancer, 
-                    "Overall" = "All",
-                    "Female Breast" = "Breast Invasive",
-                    "Kidney and Renal Pelvis" = "Kidney/Renal Pelvis",
-                    "Oral Cavity and Pharynx" = "Oral Cavity",
-                    "Corpus and Uterus, NOS" = "Corpus Uteri and Uterus, NOS",
-                    "Cervix" = "Cervix Uteri"
-                    ))
+incidence_uscs <- incidence_uscs  %>%
+  filter(YEAR == "2013-2017",
+         RACE == "All Races",
+         SEX == "Male and Female",
+         SITE != "All Sites (comparable to ICD-O-2)") %>%
+  mutate(SITE = case_when( #variable == old ~ new,
+    SITE == "All Cancer Sites Combined" ~ "Overall",
+    SITE == "Female Breast" ~ "Breast Invasive (Female)",
+    SITE == "Cervix" ~ "Cervix Uteri (Female)",
+    SITE == "Colon and Rectum" ~ "Colorectal",
+    SITE == "Corpus and Uterus, NOS" ~ "Corpus Uteri and Uterus, NOS (Female)",
+    SITE == "Corpus" ~ "Corpus (Female)",
+    SITE == "Melanomas of the Skin" ~ "Cutaneous Melanoma",
+    SITE == "Gallbladder" ~ "Gallbladder and Other Biliary",
+    SITE == "Kidney and Renal Pelvis" ~ "Kidney/Renal Pelvis",
+    SITE == "Leukemias" ~ "Leukemia",
+    SITE == "Non-Hodgkin Lymphoma" ~ "Non-Hodgkins Lymphoma",
+    SITE == "Ovary" ~ "Ovary (Female)",
+    SITE == "Prostate" ~ "Prostate (Male)",
+    SITE == "Testis" ~ "Testis (Male)",
+    SITE == "Oral Cavity and Pharynx" ~ "Oral Cavity",
+    SITE == "Hodgkin Lymphoma" ~ "Hodgkins Lymphoma",
+    TRUE ~ as.character(SITE)
+  ),
+  SEX = case_when(
+    SEX == "Male and Female" ~ "All",
+    TRUE ~ as.character(SEX)
+  )) %>%
+  select(SITE,
+         US = AGE_ADJUSTED_RATE)
 
-# recode ADHS to match USCS ----  
-USCS_incidence_USA_AZ <- USCS_incidence_USA_AZ %>% 
-  select(Cancer = "SITE",
-         USA_AGE_ADJUSTED_RATE) %>%
-  mutate(Cancer = fct_recode(
-    Cancer,
-    "Overall" = "All Cancer Sites Combined",
-    "Colorectal" = "Colon and Rectum",
-    "Cutaneous Melanoma" = "Melanomas of the Skin",
-    "Non-Hodgkins Lymphoma" = "Non-Hodgkin Lymphoma",
-    "Leukemia" = "Leukemias",
-    "Gallbladder and Other Biliary" = "Gallbladder",
-    "Gallbladder and Other Biliary" = "Other Biliary",
-    "Hodgkins Lymphoma" = "Hodgkin Lymphoma",
+# read dataset ADHS ----
+incidence_adhs <- read_rds("data/tidy/incidence_az_catch_2013-2017_table.rds")
+
+glimpse(incidence_adhs)
+
+incidence_adhs <- incidence_adhs %>%
+  mutate(SITE = case_when( #variable == old ~ new,
+    SITE == "All" ~ "Overall",
+    TRUE ~ as.character(SITE)
   ))
 
-# combine ---- 
-incidence <- full_join(USCS_incidence_USA_AZ, ADHS_incidence_catch)
+incidence_uscs
+
+
+# combine ----
+incidence <- full_join(incidence_uscs, incidence_adhs) %>%
+  arrange(desc(Catchment))
 
 incidence_table <- incidence %>%
-  mutate(IRR_AZ = AZ_Age_Adj_Rate / USA_AGE_ADJUSTED_RATE,
-       IRR_Catch = catch_Age_Adj_Rate / USA_AGE_ADJUSTED_RATE,
-       IRR_White = catch_white_Age_Adj_Rate / USA_AGE_ADJUSTED_RATE,
-       IRR_Hisp = catch_hisp_Age_Adj_Rate / USA_AGE_ADJUSTED_RATE,
-       IRR_AI = catch_ai_Age_Adj_Rate / USA_AGE_ADJUSTED_RATE,
-       IRR_Black = catch_black_Age_Adj_Rate / USA_AGE_ADJUSTED_RATE) %>%
-  arrange(desc(catch_Age_Adj_Rate))
+  mutate(IRR_AZ = AZ / US,
+       IRR_Catch = Catchment / US,
+       IRR_White = `White Non-Hispanic` / US,
+       IRR_Hisp = `White Hispanic` / US,
+       IRR_AI = `American Indian` / US,
+       IRR_Black = Black / US) %>%
+  arrange(desc(Catchment))
 
-write_rds(incidence_table, "data/tidy/uazcc_incidence_table.rds")
+write_csv(incidence_table, "data/tidy/uazcc_incidence_table_2013-2017.csv")
 
-# mortality ---- 
+incidence_table %>%
+  filter(IRR_Catch > 1) %>%
+  select(SITE, US, AZ, Catchment) %>%
+  gather(US, AZ, Catchment,
+         key = area,
+         value = rate) %>%
+  write_rds("data/tidy/uazcc_incidence_table_2013-2017_disparities.rds")
+
+# mortality ----
 
 mortality <- read_rds("data/tidy/combined_mortality_for_uazcc_attribute_table.rds")
 
