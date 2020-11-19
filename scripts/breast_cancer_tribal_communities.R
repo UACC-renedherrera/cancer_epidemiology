3dbd85ea870bed248f4357b4a295cbb0ad21e693
Rene Dario Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2020-11-18 17:40:36 -0700

    added comments,

--- scripts/breast_cancer_tribal_communities.R
+++ scripts/breast_cancer_tribal_communities.R
@@ -21,6 +21,7 @@ by_state %>%
 by_state %>%
   distinct(SITE)
 
+# generate table for az native american
 native_az_breast <- by_state %>%
   filter(AREA == "Arizona",
          RACE == "American Indian/Alaska Native",
@@ -32,6 +33,7 @@ native_az_breast
 
 str(by_az_county)
 
+# generate table for az native american for each county
 native_az__counties_breast <- by_az_county %>%
   filter(STATE == "AZ",
          RACE == "American Indian/Alaska Native",
@@ -55,4 +57,16 @@ native_breast <- native_breast %>%
 
 native_breast <- rbind(native_breast, catchment_breast)
 
+# save to csv for sharing
 write_csv(native_breast, "data/tidy/breast_cancer_in_tribal_communities.csv")
+
+table_native_breast <- read_csv("data/tidy/breast_cancer_in_tribal_communities.csv")
+
+table_native_breast %>%
+  drop_na() %>%
+  arrange(EVENT_TYPE, AREA)
+
+# Pascua Yaqui Tribe 
+# https://www.pascuayaqui-nsn.gov/Documents/2016_PYT_CHA.pdf
+# no data specific to breast cancer incidence or mortality 
+
