377b48e8b195e544875fe24d20212cf8c73d719b
Rene Dario Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2021-08-17 17:50:46 -0700

    theme, data viz

--- scripts/seer_lung_catchment_tidy.R
+++ scripts/seer_lung_catchment_tidy.R
@@ -4,7 +4,34 @@
 # load packages
 library(here)
 library(tidyverse)
-# library(knitr)
+library(RColorBrewer)
+library(ggthemes)
+
+# theme ####
+# set consistent theme for graphics & data visualizations
+theme_uazcc_brand <- theme_clean(base_size = 16) +
+  theme(
+    text = element_text(
+      family = "sans",
+      # face = "bold",
+      color = "#001C48",
+      # size = rel(1.5)
+    ),
+    panel.background = element_rect(fill = "white"),
+    panel.grid = element_line(color = "#1E5288"),
+    plot.background = element_rect(fill = "#EcE9EB"),
+    aspect.ratio = 9 / 16,
+    legend.background = element_rect(fill = "white"),
+    legend.position = "right",
+    plot.caption = element_text(size = 8),
+    # plot.subtitle = element_text(size = 12),
+    # plot.title = element_text(size = 14),
+    strip.background = element_rect(fill = "#EcE9EB")
+  )
+
+
+# this needs to be rates not cases; 
+# get the correct data 
 
 #### MORTALITY UAZCC Catchment ####
 #
@@ -26,3 +53,11 @@ lung_mort <- read_delim("data/raw/seer_stat/mortality_catchment_lung_cancer.txt"
 )
 
 lung_mort
+
+# plot 
+lung_mort %>%
+  filter(race != "All races",
+         sex != "Male and female") %>%
+  drop_na() %>%
+  ggplot(mapping = aes(x = race, y = estimate)) %>%
+  geom_bar(mapping = aes(fill = sex), position = "dodge")
