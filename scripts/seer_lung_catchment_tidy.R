4b097f20a799ff53f1840ca838d1dacbebdcb27a
Rene Dario Herrera <56937719+UACC-renedherrera@users.noreply.github.com>
2021-08-18 09:52:58 -0700

    read data, tidy, plot

--- scripts/seer_lung_catchment_tidy.R
+++ scripts/seer_lung_catchment_tidy.R
@@ -23,7 +23,7 @@ theme_uazcc_brand <- theme_clean(base_size = 16) +
     aspect.ratio = 9 / 16,
     legend.background = element_rect(fill = "white"),
     legend.position = "right",
-    plot.caption = element_text(size = 8),
+    plot.caption = element_text(size = 10),
     # plot.subtitle = element_text(size = 12),
     # plot.title = element_text(size = 14),
     strip.background = element_rect(fill = "#EcE9EB")
@@ -33,22 +33,34 @@ theme_uazcc_brand <- theme_clean(base_size = 16) +
 # this needs to be rates not cases; 
 # get the correct data 
 
-#### MORTALITY UAZCC Catchment ####
-#
-# citation: 
+#### lung MORTALITY UAZCC Catchment ####
+# {Age at Death.Age recode with <1 year olds} = '00 years','01-04 years','05-09 years','10-14 years','15-19 years','20-24 years','25-29 years','30-34 years','35-39 years','40-44 years','45-49 years','50-54 years','55-59 years','60-64 years','65-69 years','70-74 years','75-79 years','80-84 years','85+ years','Unknown'
+# {Race, Sex, Year Dth, State, Cnty, Reg.Race recode (White, Black, Other)} = 'All races','  White','  Black','  Other (American Indian/AK Native, Asian/Pacific Islander)','  Other unspecified (1978-1991)'
+# AND {Race, Sex, Year Dth, State, Cnty, Reg.Sex} = 'Male and female','  Male','  Female'
+# AND {Race, Sex, Year Dth, State, Cnty, Reg.Year of death recode} = '2015-2019'
+# AND {Race, Sex, Year Dth, State, Cnty, Reg.State} = 'Arizona'
+# AND {Race, Sex, Year Dth, State, Cnty, Reg.State-county} = '  AZ: Cochise County (04003)','  AZ: Pima County (04019)','  AZ: Pinal County (04021)','  AZ: Santa Cruz County (04023)','    AZ: Yuma County (04027) - 1994+'
+# {Site and Morphology.Cause of death recode} = '      Lung and Bronchus'
 
-lung_mort <- read_delim("data/raw/seer_stat/mortality_catchment_lung_cancer.txt",
+#
+# citation: underlying mortality data by NCHS 
+# Surveillance, Epidemiology, and End Results (SEER) Program (www.seer.cancer.gov) SEER*Stat Database: Mortality - All COD, Aggregated With County, Total U.S. (1969-2019) <Katrina/Rita Population Adjustment> - Linked To County Attributes - Total U.S., 1969-2019 Counties, National Cancer Institute, DCCPS, Surveillance Research Program, released April 2021.  Underlying mortality data provided by NCHS (www.cdc.gov/nchs).
+
+lung_mort <- read_delim("data/raw/seer_stat/mortality_lung_catchment_2014-2018_by_race.txt",
                                       delim = ",",
                                       col_names = c(
                                         "race",
                                         "sex",
-                                        "estimate"
+                                        "rate",
+                                        "count",
+                                        "population"
                                       ),
                                       col_types = cols(
                                         "race" = col_factor(),
                                         "sex" = col_factor(),
-                                        "estimate" = col_number()
-                                      ),
+                                        "rate" = col_number(),
+                                        "count" = col_number(),
+                                        "population" = col_number()),
                                       na = c("", "^", "NA")
 )
 
@@ -59,5 +71,18 @@ lung_mort %>%
   filter(race != "All races",
          sex != "Male and female") %>%
   drop_na() %>%
-  ggplot(mapping = aes(x = race, y = estimate)) %>%
-  geom_bar(mapping = aes(fill = sex), position = "dodge")
+  ggplot(mapping = aes(x = race, y = rate)) +
+  geom_col(mapping = aes(fill = sex), position = "dodge") +
+  labs(title = "Lung Cancer Mortality in the UA Catchment",
+       subtitle = "Years 2015-2019",
+       x = "Race",
+       y = "Age Adjusted Rate per 100,000",
+       fill = "Sex",
+       caption = "Source: SEER*Stat Database: Mortality - All COD, Aggregated With County, Total U.S. (1969-2019). 
+       Underlying mortality data provided by NCHS (www.cdc.gov/nchs).") +
+  scale_fill_brewer(palette = "Dark2") +
+  theme_uazcc_brand
+
+ggsave("figures/charts/uazcc_lung_mortality_2015-2019_by_race.png",
+       width = 16,
+       height = 9)
